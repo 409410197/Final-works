3cfe5db18049ac3f5234681cb211e83741ea7448
409410197 <409410197@gms.tku.edu.tw>
2021-06-15 18:27:45 +0800

    Initial commit, saving w01~w02 files

---
+++ dist/js/demo.js.js
@@ -0,0 +1,6 @@
+const p=document.querySelector('.menu-btn');
+const mainMenu=document.querySelector('.main-nav .main-menu');
+
+p.addEventListener('click',function(){
+    mainMenu.classList.toggle('show');
+});
